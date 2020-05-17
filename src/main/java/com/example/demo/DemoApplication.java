3879c845b3adf652be8d8a8685747b46d1020afc
Madhumi96 <madhumibhagya@yahoo.com>
2020-05-17 15:26:34 +0530

    Implemented Spring Security

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import com.example.demo.domain.Currency;
 import com.example.demo.domain.CurrencyRepository;
+import com.example.demo.domain.User;
+import com.example.demo.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,8 +15,11 @@ import java.math.BigDecimal;
 @SpringBootApplication
 public class DemoApplication {
 
-	@Autowired private CurrencyRepository
-	currencyRepository;
+	@Autowired
+    private CurrencyRepository currencyRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -23,11 +28,14 @@ public class DemoApplication {
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
-			currencyRepository.save(new Currency("US Dolar","USD", new BigDecimal(100), new BigDecimal(3.92)));
+		    //save demo data after start
+            userRepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG"));
+
+            currencyRepository.save(new Currency("US Dolar","USD", new BigDecimal(100), new BigDecimal(3.92)));
 			currencyRepository.save(new Currency("Euro", "EUR", new BigDecimal(300), new BigDecimal(4.52)));
 		};
 	}
 
 }
 
-//Spring example training
+
---
+++ src/main/java/com/example/demo/domain/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.domain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByUserName(String username);
+}
+
