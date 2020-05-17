379de64c33cc29b93bfd795ca74290094dd8c2dc
Madhumi96 <madhumibhagya@yahoo.com>
2020-05-17 07:47:13 +0530

    My First Commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import com.example.demo.domain.Currency;
+import com.example.demo.domain.CurrencyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@Autowired private CurrencyRepository
+	currencyRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			currencyRepository.save(new Currency("US Dolar","USD", new BigDecimal(100), new BigDecimal(3.92)));
+			currencyRepository.save(new Currency("Euro", "EUR", new BigDecimal(300), new BigDecimal(4.52)));
+		};
+	}
+
+}
